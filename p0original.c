1e44a8db1fc6be2c9cb5e889bbbb1a0905ff2aa8
Rahulsankar45 <96513391+Rahulsankar45@users.noreply.github.com>
2022-01-23 10:45:44 +0000

    pass by value

--- p0original.c
+++ p0original.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter two numbers\n");
+  scanf("%d",&a);
+  return a;
+}
+int add( int a, int b)
+{
+int k=a+b;
+return k;
+}
+void output(int a, int b, int x)
+{
+printf("%d +%d =%d\n",a,b,x);
+}
 int main()
 {
-  printf("my name is rahul\n");
+  int a=input ();
+  int b=input ();
+  int x=add (a,b);
+  output(a, b, x);
   return 0;
-} 
-
+}
