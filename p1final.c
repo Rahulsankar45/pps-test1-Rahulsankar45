36e6c4c2197f49f51ce23841f832e745919c9ceb
Rahulsankar45 <96513391+Rahulsankar45@users.noreply.github.com>
2022-01-25 12:48:27 +0000

    pass by reference

--- p1final.c
+++ p1final.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("enter two numbers\n");
+  scanf("%d%d",a,b);
+}
+void add(int a, int b,int *c)
+{
+  *c=a+b;
+}
+void output(int a,int b,int c)
+{
+  printf("%d+%d=%d",a,b,c);
+
+}
 int main()
 {
-  int a, b, sum;
-  printf("enter two numbers");
-  scanf("%d%d", &a, &b);
-  sum=a+b;
-  printf("%d +%d =%d", a, b, sum);
-  return 0;
-  
+int x,y,c;
+input(&x,&y);
+add(x,y,&c);
+output(x,y,c);
 }
