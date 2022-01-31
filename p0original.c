5af02993edf1f119f31515cbf757d0bda0a845f6
Rahulsankar45 <96513391+Rahulsankar45@users.noreply.github.com>
2022-01-31 05:10:14 +0000

    p0original.c

--- p0original.c
+++ p0original.c
@@ -1,25 +1,6 @@
 #include<stdio.h>
-int input()
-{
-  int a;
-  printf("enter two numbers\n");
-  scanf("%d",&a);
-  return a;
-}
-int add( int a, int b)
-{
-int k=a+b;
-return k;
-}
-void output(int a, int b, int x)
-{
-printf("%d +%d =%d\n",a,b,x);
-}
 int main()
 {
-  int a=input ();
-  int b=input ();
-  int x=add (a,b);
-  output(a, b, x);
+  printf("my name is rahul\n");
   return 0;
-}
+} 
