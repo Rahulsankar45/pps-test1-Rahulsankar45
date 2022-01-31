8fb12e38e2d43c25f4807abe407ae3f676795596
Rahulsankar45 <96513391+Rahulsankar45@users.noreply.github.com>
2022-01-31 05:24:18 +0000

    p1fina.c gdb

--- p1final.c
+++ p1final.c
@@ -2,10 +2,10 @@
 int main()
 {
   int a, b, sum;
-  printf("enter two numbers");
+  printf("enter two numbers\n");
   scanf("%d%d", &a, &b);
   sum=a+b;
-  printf("%d +%d =%d", a, b, sum);
+  printf("%d +%d =%d\n", a, b, sum);
   return 0;
   
 }
